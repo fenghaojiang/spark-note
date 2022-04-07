3b44578818bd44d2c596813dcbc042e0c62041bb
fenghaojiang <fenghaojiang97@m.scnu.edu.cn>
2022-04-07 09:38:19 +0800

    update

--- code/helloworld.scala
+++ code/helloworld.scala
@@ -1,3 +1,8 @@
+//scalac helloworld.scala
+//编译后我们可以看到目录下生成了 HelloWorld.class 文件
+//使用以下命令执行
+//scala HelloWorld
+
 object HelloWorld {
     def main(args:Array[String]): Unit = {
         println("Hello, world")
